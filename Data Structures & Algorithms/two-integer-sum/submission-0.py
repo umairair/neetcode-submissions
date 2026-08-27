262b7d0605b408764c2540e8a6c9ed3cd7415aed
Umair Malik <78991800+umairair@users.noreply.github.com>
2026-08-27 13:22:10 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # every input has exactly one pair of numbers that add up to the target,
+        # we need to find it.
+        # specifically the indexes of these 2 numbers in the list with the smaller
+        # one first like output example: [0,1]
+        differences = {}
+
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if difference in differences:
+                return [differences[difference], i]
+            else:
+                differences[nums[i]] = i
+                
+        return [0,0]
